c22602a9806fc90b6d48d68146d66a773665f78f
Viktor Tsymbalyuk <Viktor.Tsymbalyuk@gmail.com>
2024-07-28 13:36:56 +0300

    first commit

---
+++ ota_update.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+# -------------- install arduino-cli
+# curl -fsSL https://downloads.arduino.cc/arduino-cli/arduino-cli_latest_Linux_64bit.tar.gz -o arduino-cli.tar.gz
+# tar -xzf arduino-cli.tar.gz
+# sudo mv arduino-cli /usr/local/bin/
+# arduino-cli version
+
+# --------------- config init
+# arduino-cli config init
+# vi /home/vic/.arduino15/arduino-cli.yaml
+# # vic@HA2:~/ota_update$ cat /home/vic/.arduino15/arduino-cli.yaml
+# # board_manager:
+# #   additional_urls:
+# #     - http://arduino.esp8266.com/stable/package_esp8266com_index.json
+# arduino-cli core update-index
+# arduino-cli core search esp8266
+# arduino-cli core install esp8266:esp8266
+
+# ------------------ clone and update repo with code
+# git clone git@github.com:victron/bath_fan.git
+# add secrets.h
+# add secrets.sh
+
+
+# ------------------- install custom libs
+# arduino-cli core install home-assistant-integration
+# arduino-cli lib install "Adafruit BME280 Library"
+project_name=${PWD##*/}
+. ./secrets.sh
+espota='/home/vic/.arduino15/packages/esp8266/hardware/esp8266/3.1.2/tools/espota.py'
+build_cache='~/ota_update/build_cache'
+builds_dir='~/ota_update/build'
+
+git pull
+git log -1
+echo "------- start combile ---------"
+arduino-cli compile --fqbn esp8266:esp8266:nodemcuv2 --build-cache-path ${build_cache} --output-dir ${builds_dir} ./${project_name}.ino
+if [ $? -ne 0 ]; then
+  echo "Команда завершилась з помилкою. Завершення скрипта."
+  exit 1
+fi
+python3 ${espota} --ip ${DEVICE_IP} --auth=${OTA_PASSWORD} --file ${builds_dir}/${project_name}.ino.bin
+
+
---
+++ toilet_fan.ino
@@ -0,0 +1,123 @@
+#include <Arduino.h>
+#include <ArduinoHA.h>
+#include <ESP8266WiFi.h>
+#include <Wire.h>
+
+#include "OTAHandler.h"
+#include "button.h"
+#include "secrets.h"
+
+#define HOSTNAME "toilet_fan"
+#define SSR_PIN 0
+#define BUTTON_PIN 1  // не використовується
+
+WiFiClient client;
+HADevice device(HOSTNAME);
+HAMqtt mqtt(client, device);
+HASwitch fanSwitch("fan_switch");
+
+// можливо потрібно мати індувідуальний id для entities
+HASensorNumber wifiRssi("wifiRssi", HASensorNumber::PrecisionP0);
+
+// Створюємо об'єкт кнопки
+button btn(BUTTON_PIN);
+
+void setupWiFi() {
+  // template function
+  delay(10);
+  Serial.println();
+  Serial.print("connecting to ");
+  Serial.println(WIFI_SSID);
+
+  WiFi.hostname(HOSTNAME);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  while(WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void onSwitchCommand(bool state, HASwitch *sender) {
+  // SSR only, instead MSR
+  digitalWrite(SSR_PIN, (state ? HIGH : LOW));
+  sender->setState(state);  // report state back to the Home Assistant
+}
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Booting...");
+
+  setupWiFi();
+
+  // Логування розміру флеш-пам'яті
+  uint32_t flashSize = ESP.getFlashChipRealSize();
+  Serial.print("Flash size: ");
+  Serial.print(flashSize);
+  Serial.println(" bytes");
+
+  // set device's details (optional)
+  device.setName(HOSTNAME);
+  device.setSoftwareVersion("1.0.0");
+
+  pinMode(SSR_PIN, OUTPUT);
+  digitalWrite(SSR_PIN, LOW);
+
+  // set icon (optional)
+  fanSwitch.setIcon("mdi:fan");
+  fanSwitch.setName("Toilet Fan");
+
+  // configure sensors
+  wifiRssi.setIcon("mdi:wifi");
+  wifiRssi.setName("WIFI RSSI");
+  wifiRssi.setUnitOfMeasurement("dBm");
+
+  // handle switch state
+  fanSwitch.onCommand(onSwitchCommand);
+
+  mqtt.begin(BROKER_ADDR, MQTT_USERNAME, MQTT_PASSWORD);
+  device.enableSharedAvailability();
+  device.enableLastWill();
+
+  // Ініціалізація OTA з паролем
+  setupOTA(HOSTNAME, OTA_PASSWORD);
+}
+
+unsigned long lastUpdateAt = 0;             // Змінна для зберігання часу останнього оновлення
+const unsigned long updateInterval = 3000;  // Інтервал оновлення в мілісекундах (3000 мс = 3 секунди)
+unsigned int wifi_fail_counter = 0;
+const unsigned int wifi_fail_triger = 300000;  // при кількості спроб реконнест
+void loop() {
+  // Перевірка WiFi з'єднання
+  if(WiFi.status() != WL_CONNECTED && wifi_fail_counter > wifi_fail_triger) {
+    Serial.println("WiFi lost, trying to reconnect...");
+    setupWiFi();
+  }
+  if(WiFi.status() != WL_CONNECTED) {
+    wifi_fail_counter++;
+    Serial.print("WiFi lost, counter=");
+    Serial.println(wifi_fail_counter);
+    return;
+  }
+
+  mqtt.loop();
+  ArduinoOTA.handle();
+
+  // Перевірка натискання кнопки
+  if(btn.click()) {
+    // Зміна стану перемикача
+    onSwitchCommand(!fanSwitch.getCurrentState(), &fanSwitch);
+  }
+
+  // Перевіряємо, чи минув інтервал оновлення
+  if(millis() - lastUpdateAt > updateInterval) {
+    lastUpdateAt = millis();
+    // Моніторинг рівня WiFi сигналу
+    int8_t rssi = WiFi.RSSI();
+    wifiRssi.setValue(rssi);
+  }
+}
