443a3f2e9bff3170652feed13e39b5183dc5e295
Viktor Tsymbalyuk <Viktor.Tsymbalyuk@gmail.com>
2024-07-28 20:23:52 +0300

    very simple

--- toilet_fan.ino
+++ toilet_fan.ino
@@ -1,54 +1,59 @@
 #include <Arduino.h>
-#include <ArduinoHA.h>
+#include <ArduinoOTA.h>
 #include <ESP8266WiFi.h>
-#include <Wire.h>
 
-#include "OTAHandler.h"
-#include "button.h"
-#include "secrets.h"
+const char* ssid = "your-ssid";
+const char* password = "your-password";
 
-#define HOSTNAME "toilet_fan"
-
-void setupWiFi() {
-  // template function
-  delay(10);
-  Serial.println();
-  Serial.print("connecting to ");
-  Serial.println(WIFI_SSID);
-
-  WiFi.hostname(HOSTNAME);
-  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+void setup() {
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
   while(WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
-
-  Serial.println("");
   Serial.println("WiFi connected");
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
+
+  ArduinoOTA.setHostname("esp-ota");
+  ArduinoOTA.setPassword("admin");
+
+  ArduinoOTA.onStart([]() {
+    String type;
+    if(ArduinoOTA.getCommand() == U_FLASH) {
+      type = "sketch";
+    } else {
+      type = "filesystem";
+    }
+    Serial.println("Start updating " + type);
+  });
+
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if(error == OTA_AUTH_ERROR) {
+      Serial.println("Auth Failed");
+    } else if(error == OTA_BEGIN_ERROR) {
+      Serial.println("Begin Failed");
+    } else if(error == OTA_CONNECT_ERROR) {
+      Serial.println("Connect Failed");
+    } else if(error == OTA_RECEIVE_ERROR) {
+      Serial.println("Receive Failed");
+    } else if(error == OTA_END_ERROR) {
+      Serial.println("End Failed");
+    }
+  });
+
+  ArduinoOTA.begin();
+  Serial.println("OTA Ready");
 }
 
-void setup() {
-  Serial.begin(115200);
-  Serial.println("Booting...");
-
-  setupWiFi();
-
-  // Логування розміру флеш-пам'яті
-  uint32_t flashSize = ESP.getFlashChipRealSize();
-  Serial.print("Flash size: ");
-  Serial.print(flashSize);
-  Serial.println(" bytes");
-
-  // Ініціалізація OTA з паролем
-  setupOTA(HOSTNAME, OTA_PASSWORD);
-}
-
-unsigned long lastUpdateAt = 0;             // Змінна для зберігання часу останнього оновлення
-const unsigned long updateInterval = 3000;  // Інтервал оновлення в мілісекундах (3000 мс = 3 секунди)
-unsigned int wifi_fail_counter = 0;
-const unsigned int wifi_fail_triger = 300000;  // при кількості спроб реконнест
 void loop() {
   ArduinoOTA.handle();
 }
